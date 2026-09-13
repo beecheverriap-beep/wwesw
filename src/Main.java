4217e6c2be22ab5fe54cde1c68645ee93b3abe97
beecheverriap-beep <be.echeverriap@duocuc.cl>
2026-09-13 14:31:59 -0300

    Segundo Comentario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        Proyector proyector = new Proyector("Blessd", false);
+        Computador computador = new Computador("Acapel", true);
+        Impresora impresora = new Impresora("Hola", false);
+
         Dispositivo dispositivo = new Dispositivo("Hola", true);
+
+
+        proyector.mostrarEstado();
+        computador.mostrarEstado();
+        impresora.mostrarEstado();
+
         dispositivo.mostrarEstado();
         dispositivo.ejecutarDiagnostico();
     }
